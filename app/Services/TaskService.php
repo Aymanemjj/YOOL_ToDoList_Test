917ab84f2cb4ea63e0dd3d8f79933a09e3c70915
Aymane <aymanemj@proton.me>
2026-04-08 09:04:29 +0100

    TaskService for buiseness logic

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -2,13 +2,45 @@
 
 namespace App\Services;
 
+use App\Models\Task;
+
+use function PHPUnit\Framework\isEmpty;
+
 class TaskService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function list($filters = [])
     {
-        //
+        $query = Task::query();
+
+        if (!empty($filters['status'])) {
+            $query->where('status', $filters['status']);
+        }
+
+        if (!empty($filters['search'])) {
+            $query->where('title', 'like', "%{$filters['search']}%");
+        }
+
+        return $query->paginate(10)->withQueryString();
+    }
+
+    public function create(array $data)
+    {
+        return Task::create($data);
+    }
+
+    public function update(Task $task, array $data)
+    {
+        $task->update($data);
+        return $task->fresh();
+    }
+
+    public function delete(Task $task)
+    {
+        $task->delete();
+    }
+
+    public function markAsDone(Task $task)
+    {
+        $task->update(['status' => 'done']);
     }
 }
