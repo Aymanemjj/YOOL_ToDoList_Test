9c4109c0c9fce181a959a4ff9c2ceaf3fb9de762
Aymane <aymanemj@proton.me>
2026-04-08 10:54:08 +0100

    changed task factory to also fill user_id

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,18 +2,21 @@
 
 namespace Database\Factories;
 
-use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TaskFactory extends Factory
 {
     public function definition(): array
     {
+        $ids = User::pluck('id');
+
         return [
             'title'       => fake()->sentence(4),
             'description' => fake()->paragraph(),
             'status'      => fake()->randomElement(['todo', 'in_progress', 'done']),
             'due_date'    => fake()->dateTimeBetween('now', '+30 days')->format('Y-m-d'),
+            'user_id'     => $ids->random(),
         ];
     }
 }
