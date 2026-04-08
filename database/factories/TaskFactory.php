1b6873bcbf3e51110fdd1343746a5bb088f65989
Aymane <aymanemj@proton.me>
2026-04-08 09:05:34 +0100

    Tasks facotry and seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title'       => fake()->sentence(4),
+            'description' => fake()->paragraph(),
+            'status'      => fake()->randomElement(['todo', 'in_progress', 'done']),
+            'due_date'    => fake()->dateTimeBetween('now', '+30 days')->format('Y-m-d'),
+        ];
+    }
+}
